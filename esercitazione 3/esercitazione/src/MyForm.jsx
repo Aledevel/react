43fe8335119987bc42f9ca74cba8b2c5fdca003d
AleDevel <alessiod651@gmail.com>
2024-02-12 10:19:15 +0100

    esercizio

--- esercitazione 3/esercitazione/src/MyForm.jsx
+++ esercitazione 3/esercitazione/src/MyForm.jsx
@@ -5,20 +5,24 @@ export function MyForm(){
     const [data, setData]= useState({
         username:'',
         password:'',
+        session: false
     })
 
  function handleDataChange(event){
     const name= event.target.name
     const value=  event.target.value
+    const checked= event.target.checked
+    const type = event.target.type
     
-    setData((d) =>{
+    setData((data) =>{
          
             return{
-            ...d,
-            [name]:value
+            ...data,
+            [name]: type ==='checkbox'?checked:value,
         }
         
     })
+
     
     setData(value)
  }
@@ -27,6 +31,7 @@ export function MyForm(){
     setData({
         username:'',
         password:'',
+        session:false
     })
 }
 
@@ -35,7 +40,10 @@ return(
         <h1>Il Mio form</h1>
         <input name="Username" value={data.username} onChange={handleDataChange} />
         <input name="password" type="password" value={data.password} onChange={handleDataChange} />
+        <input type="checkbox" name="session" checked={data.session} onChange={handleDataChange}/>
+        <button disabled={!data.username || !data.password}>Log IN</button>
         <button onClick={Resetform}>Reset</button>
+      
     </div>
 )
 
