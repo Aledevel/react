67dcd4495dea187d61ac5e67ab8799131fa619a7
AleDevel <alessiod651@gmail.com>
2024-02-13 15:47:22 +0100

    esercizio ToDo List

--- esercitazione 3/esercitazione/src/App.jsx
+++ esercitazione 3/esercitazione/src/App.jsx
@@ -1,11 +1,11 @@
-import { Header } from "./Header"
-import { Image } from "./Image"
+// import { Header } from "./Header"
+// import { Image } from "./Image"
 // import { Footer } from "./Footer"
 // import { Counter } from "./Counter"
 // import { Visibility } from "./Visibility"
 // import { GeolocationViewer } from "./GeolocationViewer"
 // import { MouseClick } from "./MouseClick"
-import { Clock } from "./Clock"
+// import { Clock } from "./Clock"
 // import { UncontrolledForm } from "./UncontrolledForm"
 // import { List } from "./List"
 import { TodoList } from "./TodoList"
@@ -13,10 +13,10 @@ import { TodoList } from "./TodoList"
 export function App(){
     return(
         <div>
-          <Header/> 
-           <Image src="https://upload.wikimedia.org/wikipedia/commons/9/96/Google_web_search.png"  />
+          {/* <Header/> 
+           <Image src="https://upload.wikimedia.org/wikipedia/commons/9/96/Google_web_search.png"  /> */}
            {/* <Footer/> */}
-           <Clock/>
+           {/* <Clock/> */}
            {/* <Counter />
           <Visibility />
           <GeolocationViewer />
